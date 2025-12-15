7b4dd233c009f9fa534b16253dded9ebdc3e315f
Rhys McClew <rhysmcclew@outlook.com>
2025-12-15 16:03:47 +0000

    logging data variable fix

--- honeypot.py
+++ honeypot.py
@@ -567,7 +567,7 @@ class Mail:
 			finally:
 				connection.quit()
 		except:
-			print(Fore.RED + "[ERR]" + Style.RESET_ALL + " Mail failed to send." + captured_data_log)
+			print(Fore.RED + "[ERR]" + Style.RESET_ALL + " Mail failed to send." + log_message)
 
 class TcpHoneypot:
 	def __init__(self):
